e7946d20910040297856ef32f36c82888797c0e5
Tim Dobson <hello@tdobson.net>
2024-11-10 17:39:01 +0000

    refactor: separate Tailwind config and styles into dedicated files

--- pages/index.tsx
+++ pages/index.tsx
@@ -121,3 +121,9 @@ module.exports = {
 @tailwind base;
 @tailwind components;
 @tailwind utilities;
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
