a8066ab4f4ecd7cf5ca5efb7b949826ffe5ac850
Tim Dobson <hello@tdobson.net>
2024-11-26 20:12:16 +0000

    feat: Update event date to Saturday 30th Nov

--- pages/index.tsx
+++ pages/index.tsx
@@ -118,7 +118,7 @@ const PlayDateInvitation: React.FC = () => {
 					<div className="flex justify-center space-x-6 text-teal-50 mb-4">
 						<div className="flex items-center">
 							<Calendar className="w-5 h-5 mr-2" />
-							<span>Saturday 16th Nov</span>
+							<span>Saturday 30th Nov</span>
 						</div>
 						<div className="flex items-center">
 							<Clock className="w-5 h-5 mr-2" />
